1ccb9fa9eaccc7a611f2df14b63f5e0a52ca3004
Dhanush C P <119944896+chande-dhanush@users.noreply.github.com>
2025-03-31 17:31:08 +0530

    Update __init__.py

--- Vehicle_maintainence/init_app/__init__.py
+++ Vehicle_maintainence/init_app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY']='joemama'
+    app.config['SECRET_KEY']='secret'
     from .views import views
     from .auth import auth
     from .database import database
